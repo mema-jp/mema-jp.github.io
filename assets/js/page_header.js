38b63689a742ff21b0e9aa52e419a48043049e94
duan erlong <erlong.duan@gmail.com>
2022-11-26 22:34:08 +0900

    fix button

--- assets/js/page_header.js
+++ assets/js/page_header.js
@@ -8,6 +8,33 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var select = function select(el) {
+  var all = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+  el = el.trim();
+  if (all) {
+    return [].concat(_toConsumableArray(document.querySelectorAll(el)));
+  } else {
+    return document.querySelector(el);
+  }
+};
+var on = function on(type, el, listener) {
+  var all = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
+
+  var selectEl = select(el, all);
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(function (e) {
+        return e.addEventListener(type, listener);
+      });
+    } else {
+      selectEl.addEventListener(type, listener);
+    }
+  }
+};
+
 var PageHeader = function (_React$Component) {
   _inherits(PageHeader, _React$Component);
 
@@ -28,6 +55,11 @@ var PageHeader = function (_React$Component) {
         pathname = "index.html";
       }
       this.setState({ active: pathname });
+      on('click', '.mobile-nav-toggle', function (e) {
+        select('#navbar').classList.toggle('navbar-mobile');
+        this.classList.toggle('bi-list');
+        this.classList.toggle('bi-x');
+      });
     }
   }, {
     key: "render",
--- src/page_header.js
+++ src/page_header.js
@@ -1,5 +1,22 @@
-'use strict';
-
+'use strict';  
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
+    }
+  }
+}
 class PageHeader extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +28,11 @@ class PageHeader extends React.Component {
       pathname = "index.html"
     }
     this.setState({ active: pathname });
+    on('click', '.mobile-nav-toggle', function (e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
   }
 
   render() {
